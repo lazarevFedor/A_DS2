c592ececd68270687dc373c0e8b316b6b8fd7c33
lazarevFedor <lazarevfedor@gmail.com>
2024-10-23 11:09:10 +0300

    Косметика

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 func main() {
 	arr := make([]int, 100)
-	var tim Timsort
 	for i := 0; i < 50; i++ {
 		arr[i] = i + 1
 	}
 	for i := 50; i < 100; i++ {
 		arr[i] = 100 - i
 	}
-	arr = tim.Sort(arr)
+	arr = Sort(arr)
 }
