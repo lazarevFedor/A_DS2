57a3b1bd6bc1de66b5f7a079c52b72451dde72c7
lazarevFedor <lazarevfedor@gmail.com>
2024-10-25 13:30:53 +0300

    Ввод и вывод массива

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	arr := make([]int, 100)
 	for i := 0; i < 50; i++ {
@@ -9,4 +11,7 @@ func main() {
 		arr[i] = 100 - i
 	}
 	arr = Sort(arr)
+	for i := 0; i < 100; i++ {
+		fmt.Printf("%d ", arr[i])
+	}
 }
