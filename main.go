3e369ee5671193667508d86d59aa4d5cc2895d5e
lazarevFedor <lazarevfedor@gmail.com>
2024-10-25 20:15:06 +0300

    Testing changes

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
 	arr := make([]int, 100)
@@ -8,9 +11,13 @@ func main() {
 		arr[i] = i + 1
 	}
 	for i := 50; i < 100; i++ {
-		arr[i] = 100 - i
+		arr[i] = rand.Intn(100)
+	}
+	for i := 0; i < 100; i++ {
+		fmt.Printf("%d ", arr[i])
 	}
 	arr = Sort(arr)
+	fmt.Println("\nSorted array:")
 	for i := 0; i < 100; i++ {
 		fmt.Printf("%d ", arr[i])
 	}
